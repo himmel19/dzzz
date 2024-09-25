59a9d5ac46229f9f4b27aa6e0fecc26f510c0599
Andrey <andreykim@MacBookAndrey.local>
2024-09-25 13:14:01 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,30 @@
+const body = document.body;
+const q = document.querySelector(".q");
+const w = document.querySelector(".w");
+const e = document.querySelector(".e");
+const r = document.querySelector(".r");
+const t = document.querySelector(".t");
+const originalBrick = document.querySelector('.original');
+
+
+q.addEventListener("click", () => {
+  originalBrick.style.background = "url(https://www.funnyart.club/uploads/posts/2022-12/thumbs/1671275788_www-funnyart-club-p-kartinki-burgerov-krasivo-22.jpg)";
+});
+
+w.addEventListener("click", () => {
+  originalBrick.style.background = "url(https://www.funnyart.club/uploads/posts/2022-12/thumbs/1671275811_www-funnyart-club-p-kartinki-burgerov-krasivo-7.jpg)";
+});
+
+e.addEventListener("click", () => {
+  originalBrick.style.background = "url(https://www.funnyart.club/uploads/posts/2022-12/thumbs/1671275823_www-funnyart-club-p-kartinki-burgerov-krasivo-20.jpg)";
+});
+
+r.addEventListener("click", () => {
+  originalBrick.style.background = "url(https://64.media.tumblr.com/4016871a46b31ef7c313a7eec3522b65/tumblr_nxbuw6JpMr1qflpc1o1_1280.jpg)";
+});
+
+t.addEventListener("click", () => {
+  originalBrick.style.background = "url(https://i.pinimg.com/originals/29/14/2b/29142b39f9e347f03804a118b2f4c371.png)";
+});
+
+
